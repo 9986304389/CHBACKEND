3cb79f00a849f3af1296e3386b0076abad936e42
velpulakavitha <kavitha.v@medvol.in>
2024-01-19 23:52:24 +0530

    index

--- index.js
+++ index.js
@@ -4,21 +4,35 @@ const serverless = require('serverless-http');
 const app = express();
 const port = 3001;
 
+// const pool = new Pool({
+//   user: 'postgres',
+//   host: 'localhost',
+//   database: 'postgres',
+//   password: 'KAVItha@123',
+//   port: 5432,
+//   ssl: {
+//     rejectUnauthorized: false,
+//   },
+//});
+
+
 const isSSL = false;
 const pool = new Pool({
-  user: 'postgres',
-  host: 'localhost',
-  database: 'postgres',
-  password: 'KAVItha@123',
-  port: 5432,
-  ssl: {
-    rejectUnauthorized: false,
-  },
-  ssl: isSSL ? { rejectUnauthorized: false } : false,
+    connectionString: 'postgres://postgres:KAVItha@123@localhost:5432/postgres',
+    ssl: {
+        rejectUnauthorized: false,
+    },
+    ssl: isSSL ? { rejectUnauthorized: false } : false,
 });
 
-
-
+// pool.query('SELECT NOW()', (err, res) => {
+//     if (err) {
+//         console.error('Error connecting to the database:', err);
+//     } else {
+//         console.log('Connected to the database. Current timestamp:', res.rows[0].now);
+//     }
+//     pool.end();
+// });
 
 app.get('/test', async (req, res) => {
   try {
