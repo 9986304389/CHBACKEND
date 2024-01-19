662145594368b39acb05e85b2914106d92901da2
velpulakavitha <kavitha.v@medvol.in>
2024-01-19 23:38:45 +0530

    index

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ const pool = new Pool({
   },
 });
 
-app.get('/t', async (req, res) => {
+app.get('/test', async (req, res) => {
   try {
     const result = await pool.query('SELECT * FROM login_user');
     res.json(result.rows);
