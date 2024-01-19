33ab2211a2fc5f68fbe2097bf3b45320ace9c66b
velpulakavitha <kavitha.v@medvol.in>
2024-01-19 22:34:14 +0530

    inital data

---
+++ index.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const { Pool } = require('pg');
+const serverless = require('serverless-http');
+const app = express();
+const port = 3001;
+
+const pool = new Pool({
+  user: 'postgres',
+  host: 'localhost',
+  database: 'postgres',
+  password: 'KAVItha@123',
+  port: 5432,
+});
+
+app.get('/api/data', async (req, res) => {
+  try {
+    const result = await pool.query('SELECT * FROM login_user');
+    res.json(result.rows);
+  } catch (error) {
+    console.error('Error executing SQL query', error);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
+
+// app.listen(port, () => {
+//   console.log(`Server is running on port ${port}`);
+// });
+
+
+module.exports.handler = serverless(app);
